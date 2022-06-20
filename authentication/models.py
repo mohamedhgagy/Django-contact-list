2c37e2f1fee277dc5cc86aada67cd582e885b5ac
mohamedhgagy <m.hagagy@laplacesoftware.com>
2022-06-21 00:27:12 +0200

    [ADD] urls.py, serializers.py in authentication app and create endpoint to register a new user with serializer custom validation

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,10 @@
-from django.db import models
-
+from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+# class Contact(models.Model):
+#     username = models.ForeignKey(User, on_delete=models.CASCADE)
+#     firts_name = models.CharField(max_length=100)
+#     last_name = models.CharField(max_length=100)
+#     mobile = models.CharField(max_length=11)
+# class CustomUsers(models.AbstractUser):
+#     ...
